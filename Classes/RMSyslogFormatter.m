4844d4fb2510364e47643300096f77a8834f098c
David Ametsitsi <david@ametsitsi.com>
2015-04-27 01:23:53 -0700

    cocoalumberjack compat fixes
    https://bitbucket.org/luisrecuenco/papertraillumberjack/src/e42a6d200191
    b24e2e02f551bf674aff8c4c9101/Classes/RMSyslogFormatter.m

--- Classes/RMSyslogFormatter.m
+++ Classes/RMSyslogFormatter.m
@@ -20,20 +20,21 @@ static NSString * const RMAppUUIDKey = @"RMAppUUIDKey";
     NSString *logLevel;
     switch (logMessage.flag)
     {
-        case DDLogFlagError     : logLevel = @"11"; break;
-        case DDLogFlagWarning   : logLevel = @"12"; break;
+        case DDLogFlagError    : logLevel = @"11"; break;
+        case DDLogFlagWarning      : logLevel = @"12"; break;
         case DDLogFlagInfo      : logLevel = @"14"; break;
-        case DDLogFlagDebug     : logLevel = @"15"; break;
-        case DDLogFlagVerbose   : logLevel = @"15"; break;
+        case DDLogFlagDebug    : logLevel = @"15"; break;
+        case DDLogFlagVerbose   : logLevel = @"15" ; break;
         default                 : logLevel = @"15"; break;
     }
     
-    //Also display the file the logging occurred in to ease later debugging
-    NSString *file = [[logMessage.file lastPathComponent] stringByDeletingPathExtension];
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"MMM dd HH:mm:ss"];
-    [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+    static NSDateFormatter *dateFormatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"MMM dd HH:mm:ss"];
+        [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+    });
     NSString *timestamp = [dateFormatter stringFromDate:logMessage.timestamp];
     
     //Get vendor id
@@ -42,22 +43,38 @@ static NSString * const RMAppUUIDKey = @"RMAppUUIDKey";
     //Get program name
     NSString *programName = [self programName];
     
-    NSString *log = [NSString stringWithFormat:@"<%@>%@ %@ %@: %@ %@@%@@%lu \"%@\"", logLevel, timestamp, machineName, programName, logMessage.threadID, file, logMessage.function, (unsigned long)logMessage.line, msg];
+    NSString *log = [NSString stringWithFormat:@"<%@>%@ %@ %@: %@ %@@%@@%lu \"%@\"", logLevel, timestamp, machineName, programName, logMessage.threadID, logMessage.fileName, logMessage.function, (unsigned long)logMessage.line, msg];
     
     return log;
 }
 
 -(NSString *) machineName
 {
-    NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleExecutableKey];
-    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleVersionKey];
-
-    return [@[appName, appVersion] componentsJoinedByString:@"-"];
+    //We will generate and use a app-specific UUID to maintain user privacy.
+    NSString *uuid = [[NSUserDefaults standardUserDefaults] stringForKey:RMAppUUIDKey];
+    if (uuid == nil) {
+        uuid = [[NSUUID UUID] UUIDString];
+        [[NSUserDefaults standardUserDefaults] setObject:uuid forKey:RMAppUUIDKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+    
+    return uuid;
 }
 
 -(NSString *) programName
 {
-    return [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    NSString *programName = [[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:@"CFBundleDisplayName"];
+    if (programName == nil) {
+        programName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+    }
+    
+    //Remove all whitespace characters from appname
+    if (programName != nil) {
+        NSArray *components = [programName componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        programName = [components componentsJoinedByString:@""];
+    }
+    
+    return programName;
 }
 
 @end
